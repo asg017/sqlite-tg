05388464cd0c40fdf501893106d5f8667b3707ab
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-09-28 13:49:34 -0700

    guard subtype

--- Makefile
+++ Makefile
@@ -150,5 +150,9 @@ version:
 
 test-loadable: loadable
 	$(PYTHON) -m pytest -vv tests/test-loadable.py
+
+test-loadable-snapshot-update: loadable
+	$(PYTHON) -m pytest -vv tests/test-loadable.py --snapshot-update
+
 test-loadable-watch:
 	watchexec -w sqlite-tg.c -w tests/test-loadable.py -w docs.md -w Makefile --clear -- make test-loadable
--- tests/test-loadable.py
+++ tests/test-loadable.py
@@ -9,6 +9,7 @@ import shapely
 
 EXT_PATH = "./dist/tg0"
 
+
 def connect(ext, path=":memory:"):
     db = sqlite3.connect(path)
 
@@ -77,6 +78,7 @@ MODULES = [
     "tg_rect_parts",
 ]
 
+SUPPORTS_SUBTYPE = sqlite3.version_info[1] > 38
 
 LINE_A = shapely.from_wkt("LINESTRING (0 0, 1 1)")
 
@@ -89,7 +91,8 @@ LINE_SEPARATE = (
     shapely.from_wkt("LINESTRING (2 0, 2 2)"),
 )
 
-COLLECTION_GEOJSON = (Path(__file__).parent / "data"  / "collection.geojson").read_text()
+COLLECTION_GEOJSON = (Path(__file__).parent / "data" / "collection.geojson").read_text()
+
 
 def test_funcs():
     funcs = list(
@@ -100,20 +103,22 @@ def test_funcs():
     )
     assert funcs == FUNCTIONS
 
+
 def test_modules():
     modules = list(
-        map(
-            lambda a: a[0], db.execute("select name from loaded_modules").fetchall()
-        )
+        map(lambda a: a[0], db.execute("select name from loaded_modules").fetchall())
     )
     assert modules, MODULES
+
 
 def test_tg_version():
     assert db.execute("select tg_version()").fetchone()[0][0] == "v"
 
+
 def test_tg_debug():
     debug = db.execute("select tg_debug()").fetchone()[0].split("\n")
     assert len(debug) == 3
+
 
 def test_tg_intersects():
     tg_intersects = lambda *args: db.execute(
@@ -121,61 +126,80 @@ def test_tg_intersects():
     ).fetchone()[0]
 
     for format in ("wkt", "wkb", "geojson"):
-        assert tg_intersects(
-                *[getattr(shapely, f"to_{format}")(l) for l in LINE_CROSSES]
-            ) == 1
+        assert (
+            tg_intersects(*[getattr(shapely, f"to_{format}")(l) for l in LINE_CROSSES])
+            == 1
+        )
 
-        assert tg_intersects(
-                *[getattr(shapely, f"to_{format}")(l) for l in LINE_SEPARATE]
-            ) ==0
+        assert (
+            tg_intersects(*[getattr(shapely, f"to_{format}")(l) for l in LINE_SEPARATE])
+            == 0
+        )
 
 
 def test_tg_type():
     tg_type = lambda *args: db.execute("select tg_type(?)", args).fetchone()[0]
-    assert tg_type(shapely.to_wkt(LINE_A))== "LineString"
+    assert tg_type(shapely.to_wkt(LINE_A)) == "LineString"
 
-    assert tg_type("POINT (30 10)")== "Point"
-    assert tg_type("LINESTRING (30 10, 10 30, 40 40)")== "LineString"
-    assert tg_type("POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))")== "Polygon"
+    assert tg_type("POINT (30 10)") == "Point"
+    assert tg_type("LINESTRING (30 10, 10 30, 40 40)") == "LineString"
+    assert tg_type("POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))") == "Polygon"
     assert tg_type("MULTIPOINT (10 40, 40 30, 20 20, 30 10)") == "MultiPoint"
-    assert tg_type(
+    assert (
+        tg_type(
             "MULTIPOLYGON (((30 20, 45 40, 10 40, 30 20)),((15 5, 40 10, 10 20, 5 10, 15 5)))"
-        ) =="MultiPolygon"
-    assert tg_type(
+        )
+        == "MultiPolygon"
+    )
+    assert (
+        tg_type(
             "GEOMETRYCOLLECTION (POINT (40 10),LINESTRING (10 10, 20 20, 10 40),POLYGON ((40 40, 20 45, 45 30, 40 40)))"
-        ) =="GeometryCollection"
+        )
+        == "GeometryCollection"
+    )
 
-    db.execute(
-        "select tg_type(tg_point(1, 2)) from json_each('[1,2,3]')"
-    ).fetchall()
+    db.execute("select tg_type(tg_point(1, 2)) from json_each('[1,2,3]')").fetchall()
+
 
 def test_tg_to_wkt():
     tg_to_wkt = lambda *args: db.execute("select tg_to_wkt(?)", args).fetchone()[0]
     assert tg_to_wkt(shapely.to_wkb(LINE_A)) == "LINESTRING(0 0,1 1)"
     # TODO more tests
 
+
 def test_tg_to_wkb(snapshot):
     tg_to_wkb = lambda *args: db.execute("select tg_to_wkb(?)", args).fetchone()[0]
-    assert tg_to_wkb(shapely.to_wkt(LINE_A))== snapshot
+    assert tg_to_wkb(shapely.to_wkt(LINE_A)) == snapshot
     # TODO more tests
+
 
 def test_tg_extra_json():
     tg_extra_json = lambda *args: db.execute(
         "select tg_extra_json(?)", args
     ).fetchone()[0]
-    assert tg_extra_json('{"type": "Point","coordinates": [-118.2097812,34.0437074]}') == None
-    assert tg_extra_json(
+    assert (
+        tg_extra_json('{"type": "Point","coordinates": [-118.2097812,34.0437074]}')
+        == None
+    )
+    assert (
+        tg_extra_json(
             '{"id": "A", "type": "Point","coordinates": [-118.2097812,34.0437074]}'
-        ) == '{"id":"A"}'
+        )
+        == '{"id":"A"}'
+    )
+
 
 def test_tg_geom():
     assert db.execute("select tg_geom(?)", ["POINT(1 1)"]).fetchone()[0] == None
-    assert db.execute("select subtype(tg_geom(?))", ["POINT(1 1)"]).fetchone()[0] == 112
-    with pytest.raises(
-        sqlite3.OperationalError, match="ParseError: invalid text"
-    ):
+    if SUPPORTS_SUBTYPE:
+        assert (
+            db.execute("select subtype(tg_geom(?))", ["POINT(1 1)"]).fetchone()[0]
+            == 112
+        )
+    with pytest.raises(sqlite3.OperationalError, match="ParseError: invalid text"):
         db.execute("select tg_geom(?)", ["POINT(1 1"]).fetchone()
     # self.skipTest("TODO")
+
 
 def test_tg_valid_geojson():
     tg_valid_geojson = lambda *args: db.execute(
@@ -184,27 +208,38 @@ def test_tg_valid_geojson():
     assert tg_valid_geojson(shapely.to_geojson(LINE_A)) == 1
     assert tg_valid_geojson(shapely.to_wkt(LINE_A)) == 0
 
+
 def test_tg_valid_wkb():
-    tg_valid_wkb = lambda *args: db.execute(
-        "select tg_valid_wkb(?)", args
-    ).fetchone()[0]
+    tg_valid_wkb = lambda *args: db.execute("select tg_valid_wkb(?)", args).fetchone()[
+        0
+    ]
     assert tg_valid_wkb(shapely.to_wkb(LINE_A)) == 1
     assert tg_valid_wkb(shapely.to_wkt(LINE_A)) == 0
 
+
 def test_tg_valid_wkt():
-    tg_valid_wkt = lambda *args: db.execute(
-        "select tg_valid_wkt(?)", args
-    ).fetchone()[0]
+    tg_valid_wkt = lambda *args: db.execute("select tg_valid_wkt(?)", args).fetchone()[
+        0
+    ]
     assert tg_valid_wkt(shapely.to_wkt(LINE_A)) == 1
     assert tg_valid_wkt(shapely.to_wkb(LINE_A)) == 0
+
 
 def test_tg_geometries_each(snapshot):
     tg_geometries_each = lambda *args: execute_all(
         db, "select rowid, * from tg_geometries_each(?)", args
     )
-    assert execute_all(db, "select rowid, geometry, tg_to_geojson(geometry) from tg_geometries_each(?)", [COLLECTION_GEOJSON]) ==  snapshot
-    #with pytest.raises(sqlite3.OperationalError, match="XXX"):
+    assert (
+        execute_all(
+            db,
+            "select rowid, geometry, tg_to_geojson(geometry) from tg_geometries_each(?)",
+            [COLLECTION_GEOJSON],
+        )
+        == snapshot
+    )
+    # with pytest.raises(sqlite3.OperationalError, match="XXX"):
     #    tg_geometries_each(1)
+
 
 @pytest.mark.skip(reason="TODO")
 def test_tg_lines_each():
@@ -215,6 +250,7 @@ def test_tg_lines_each():
     with pytest.raises(sqlite3.OperationalError):
         tg_lines_each()
 
+
 @pytest.mark.skip(reason="TODO")
 def test_tg_points_each():
     tg_points_each = lambda *args: execute_all(
@@ -223,6 +259,7 @@ def test_tg_points_each():
     assert tg_points_each() == []
     with pytest.raises(sqlite3.OperationalError):
         tg_points_each()
+
 
 @pytest.mark.skip(reason="TODO")
 def test_tg_polygons_each():
@@ -233,6 +270,7 @@ def test_tg_polygons_each():
     with pytest.raises(sqlite3.OperationalError):
         tg_polygons_each()
 
+
 @pytest.mark.skip(reason="TODO")
 def test_tg_rect_parts():
     tg_rect_parts = lambda *args: execute_all(
@@ -242,39 +280,55 @@ def test_tg_rect_parts():
     with pytest.raises(sqlite3.OperationalError):
         tg_rect_parts()
 
+
 def test_tg_to_geojson():
     tg_to_geojson = lambda *args: db.execute(
         "select tg_to_geojson(?)", args
     ).fetchone()[0]
-    assert tg_to_geojson(shapely.to_wkt(LINE_A)) == '{"type":"LineString","coordinates":[[0,0],[1,1]]}'
+    assert (
+        tg_to_geojson(shapely.to_wkt(LINE_A))
+        == '{"type":"LineString","coordinates":[[0,0],[1,1]]}'
+    )
     # TODO more tests
 
+
 def test_tg_point():
-    assert db.execute("select tg_point(1, 1)").fetchone()[0] ==None
+    assert db.execute("select tg_point(1, 1)").fetchone()[0] == None
     assert db.execute("select tg_to_wkt(tg_point(1, 1))").fetchone()[0] == "POINT(1 1)"
     assert [
-            row[0]
-            for row in db.execute(
-                "select tg_to_wkt(tg_point(1, 1)) from json_each('[1,2]')"
-            ).fetchall()
-        ] ==  ["POINT(1 1)", "POINT(1 1)"]
+        row[0]
+        for row in db.execute(
+            "select tg_to_wkt(tg_point(1, 1)) from json_each('[1,2]')"
+        ).fetchall()
+    ] == ["POINT(1 1)", "POINT(1 1)"]
 
-    if sqlite3.version_info[1] > 38:
+    if SUPPORTS_SUBTYPE:
         assert db.execute("select subtype(tg_point(1, 1)").fetchone()[0] == 4
+
 
 def test_tg_point_geojson():
     tg_point_geojson = lambda *args: db.execute(
         "select tg_point_geojson(?, ?)", args
     ).fetchone()[0]
     assert tg_point_geojson(1, 2) == '{"type":"Point","coordinates":[1,2]}'
-    assert tg_point_geojson(1.111, 2.222) == '{"type":"Point","coordinates":[1.111,2.222]}'
+    assert (
+        tg_point_geojson(1.111, 2.222) == '{"type":"Point","coordinates":[1.111,2.222]}'
+    )
+
 
 def test_tg_point_wkb():
     tg_point_wkb = lambda *args: db.execute(
         "select tg_point_wkb(?, ?)", args
     ).fetchone()[0]
-    assert tg_point_wkb(1, 2) == b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@"
-    assert tg_point_wkb(1.111, 2.222) == b"\x01\x01\x00\x00\x00-\xb2\x9d\xef\xa7\xc6\xf1?-\xb2\x9d\xef\xa7\xc6\x01@"
+    assert (
+        tg_point_wkb(1, 2)
+        == b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x00@"
+    )
+    assert (
+        tg_point_wkb(1.111, 2.222)
+        == b"\x01\x01\x00\x00\x00-\xb2\x9d\xef\xa7\xc6\xf1?-\xb2\x9d\xef\xa7\xc6\x01@"
+    )
+
 
 def test_tg_point_wkt():
     tg_point_wkt = lambda *args: db.execute(
@@ -283,24 +337,29 @@ def test_tg_point_wkt():
     assert tg_point_wkt(1, 2) == "POINT(1 2)"
     assert tg_point_wkt(1.111, 2.222) == "POINT(1.111 2.222)"
 
+
 def test_tg_points_each(snapshot):
-    assert execute_all(
+    assert (
+        execute_all(
             db,
-            "select rowid, point, subtype(point), tg_to_wkt(point), tg_to_geojson(point) from tg_points_each(?)",
+            "select rowid, point, tg_to_wkt(point), tg_to_geojson(point) from tg_points_each(?)",
             ["MULTIPOINT ((10 40), (40 30), (20 20), (30 10))"],
-        ) ==  snapshot
-    assert execute_all(
+        )
+        == snapshot
+    )
+    assert (
+        execute_all(
             db,
             """
               select
                 tg_points_each.rowid,
                 point,
-                subtype(point),
                 tg_to_wkt(point),
                 tg_to_geojson(point)
               from json_each(?)
               join tg_points_each(value)
-            """,[
+            """,
+            [
                 json.dumps(
                     [
                         "MULTIPOINT ((10 20), (30 40))",
@@ -308,18 +367,26 @@ def test_tg_points_each(snapshot):
                     ]
                 )
             ],
-        ) ==        snapshot
+        )
+        == snapshot
+    )
+
 
 def test_coverate():
     current_module = inspect.getmodule(inspect.currentframe())
-    test_methods = [member[0]  for member in inspect.getmembers(current_module) if member[0].startswith("test_")]
+    test_methods = [
+        member[0]
+        for member in inspect.getmembers(current_module)
+        if member[0].startswith("test_")
+    ]
     funcs_with_tests = set([x.replace("test_", "") for x in test_methods])
     README = (Path(__file__).parent.parent / "docs.md").read_text()
     for func in [*FUNCTIONS, *MODULES]:
-        assert func in funcs_with_tests , f"{func} is not tested"
+        assert func in funcs_with_tests, f"{func} is not tested"
         assert func in README, f"{func} is not documented"
         assert f'name="{func}"' in README, f"{func} is not documented"
         assert f"{func}(" in README, f"{func} missing code sample"
+
 
 if __name__ == "__main__":
     unittest.main()
