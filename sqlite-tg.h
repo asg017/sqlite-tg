154ffd73f1225cfbaf9dd090b0db1055a511e9d7
Alex Garcia <alexsebastian.garcia@gmail.com>
2024-01-11 15:35:55 -0800

    jk that was right?

--- sqlite-tg.h
+++ sqlite-tg.h
@@ -1,6 +1,6 @@
 #include "sqlite3ext.h"
 
-#define SQLITE_TG_VERSION "v0.0.1-alpha.6"
+#define SQLITE_TG_VERSION "0.0.1-alpha.6"
 #define SQLITE_TG_DATE "2023-09-23T23:03:45Z-0700"
 #define SQLITE_TG_SOURCE "aadc33ff191e7a390dd11e89f18ff70d092a9604"
 
