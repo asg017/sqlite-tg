34ae9c519c6bbfbedc7bca76d483893b420234d5
Alex Garcia <alexsebastian.garcia@gmail.com>
2024-11-11 20:34:33 -0800

    fix tests

--- sqlite-tg.h
+++
@@ -1,9 +0,0 @@
-#include "sqlite3ext.h"
-
-#define SQLITE_TG_VERSION "v0.0.1-alpha.10"
-#define SQLITE_TG_DATE "2024-08-25T16:09:10Z-0700"
-#define SQLITE_TG_SOURCE "a55fa791793c73545f04a5b58e66da0a403de2ee"
-
-
-int sqlite3_tg_init(sqlite3 *db, char **pzErrMsg,
-                    const sqlite3_api_routines *pApi);
