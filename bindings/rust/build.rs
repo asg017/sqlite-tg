42fe736df1adba822356494ac792d193e8be2a07
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-02-28 14:52:40 -0800

    gha: windows

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,7 +1,11 @@
 fn main() {
-    cc::Build::new()
+    let build = cc::Build::new();
+    build
         .file("sqlite-tg.c")
         .file("tg.c")
-        .define("SQLITE_CORE", None)
-        .compile("sqlite_tg0");
+        .define("SQLITE_CORE", None);
+      if cfg!(target_os = "windows") {
+        build.flag("/std:c11");
+    }
+        build.compile("sqlite_tg0");
 }
