16ae0d99648c148ca162c1c6e484e2ff967f260c
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-03-01 08:33:52 -0800

    gha: atomics pls

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -5,7 +5,7 @@ fn main() {
         .file("tg.c")
         .define("SQLITE_CORE", None);
     if cfg!(target_os = "windows") {
-        build.flag("/std:c11");
+        build.flag("/D_C11_ATOMICS");
     }
     build.compile("sqlite_tg0");
 }
