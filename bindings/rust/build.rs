df5683d25e39c4c170e2b268ec1d6822b0b7c964
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-03-01 08:22:40 -0800

    fix build

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let build = cc::Build::new();
+    let mut build = cc::Build::new();
     build
         .file("sqlite-tg.c")
         .file("tg.c")
         .define("SQLITE_CORE", None);
-      if cfg!(target_os = "windows") {
+    if cfg!(target_os = "windows") {
         build.flag("/std:c11");
     }
-        build.compile("sqlite_tg0");
+    build.compile("sqlite_tg0");
 }
